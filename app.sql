b8414534f6ddae9b2e7ca89a64e21e422cd21f7c
luozyiii <924361501@qq.com>
2021-05-18 22:21:14 +0800

    更新sql

--- app.sql
+++ app.sql
@@ -2,61 +2,97 @@ create database egg_fly;
 
 use egg_fly;
 
---- 用户表
-create table `user` (
-  `id` int not null auto_increment,
-  `username` varchar(20) default null comment '用户名',
-  `password` varchar(64) default null comment '密码',
-  `avatar` text comment '头像',
-  `phone` varchar(20) default null comment '电话',
-  `sign` varchar(300) default null comment '用户签名',
-  `createTime` timestamp default null comment '创建时间',
-  `updateTime` timestamp default null comment '更新时间',
-  primary key(`id`)
-)engine=InnoDB auto_increment=1 default charset=utf8 comment='用户表';
+/*
+ Navicat Premium Data Transfer
 
---- 民宿表
-create table `house` {
-  `id` int not null auto_increment,
-  `name` varchar(50) default null comment '房屋名称',
-  `info` varchar(150) default null comment '房屋简介',
-  `address` varchar(200) default null comment '房屋地址',
-  `price` int default null comment '房屋价格',
-  `publishTime` timestamp default null comment '发布时间',
-  `cityCode` varchar(10) not null comment '城市编码',
-  `showCount` int(5) default 0 comment '展示次数',
-  `startTime` timestamp default null comment '开始出租时间',
-  `endTime` timestamp default null comment '出租结束时间',
-  primary key(`id`)
-}engine=InnoDB auto_increment=1 default charset=utf8 comment='民宿表';
+ Source Server         : mysql
+ Source Server Type    : MySQL
+ Source Server Version : 80024
+ Source Host           : localhost:3306
+ Source Schema         : egg_fly
 
--- 图片表
-create table `imgs` {
-  `id` int not null auto_increment,
-  `url` varchar(500) default null comment '图片地址',
-  `houseId` int not null comment '房屋id',
-  `createTime` timestamp default null comment '创建时间',
-  primary key(`id`)
-}engine=InnoDB auto_increment=1 default charset=utf8 comment='图片表';
+ Target Server Type    : MySQL
+ Target Server Version : 80024
+ File Encoding         : 65001
 
--- 评论表
-create table `comment` {
-  `id` int not null auto_increment,
-  `userId` int not null comment '用户表id',
-  `houseId` int not null comment '房屋表id',
-  `msg` varchar(500) default null comment '评论内容',
-  `createTime` timestamp default null comment '创建时间',
-  primary key(`id`)
-}engine=InnoDB auto_increment=1 default charset=utf8 comment='评论表';
+ Date: 18/05/2021 22:16:08
+*/
 
--- 订单表
-create table `orders`(
-  `id` int not null auto_increment,
-  `orderNumber` varchar(20) default null comment '订单编号',
-  `userId` int not null comment '用户id',
-  `houseId` int not null comment '房屋id',
-  `isPayed` int default 0 comment '是否支付，0未支付，1已支付',
-  `createTime` timestamp default null comment '创建时间',
-  `updateTime` timestamp default null comment '更新时间',
-  primary key(`id`)
-)engine=InnoDB auto_increment=1 default charset=utf8 comment='订单表';
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for comment
+-- ----------------------------
+DROP TABLE IF EXISTS `comment`;
+CREATE TABLE `comment` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `userId` int NOT NULL COMMENT '用户表id',
+  `houseId` int NOT NULL COMMENT '房屋表id',
+  `msg` varchar(500) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '评论内容',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for house
+-- ----------------------------
+DROP TABLE IF EXISTS `house`;
+CREATE TABLE `house` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '房屋名称',
+  `info` varchar(150) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '房屋简介',
+  `address` varchar(200) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '房屋地址',
+  `price` int DEFAULT NULL COMMENT '房屋价格',
+  `publishTime` datetime DEFAULT NULL COMMENT '发布时间',
+  `cityCode` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '城市编码',
+  `showCount` int NOT NULL DEFAULT '0' COMMENT '展示次数',
+  `startTime` datetime DEFAULT NULL COMMENT '开始出租时间',
+  `endTime` datetime DEFAULT NULL COMMENT '出租结束时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for imgs
+-- ----------------------------
+DROP TABLE IF EXISTS `imgs`;
+CREATE TABLE `imgs` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `url` varchar(500) DEFAULT NULL COMMENT '图片地址',
+  `houseId` int NOT NULL COMMENT '房屋id',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `orderNumber` varchar(20) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '订单编号',
+  `userId` int NOT NULL COMMENT '用户id',
+  `houseId` int NOT NULL COMMENT '房屋id',
+  `isPayed` int DEFAULT '0' COMMENT '是否支付，0未支付，1已支付',
+  `createTime` timestamp NULL DEFAULT NULL COMMENT '创建时间',
+  `updateTime` timestamp NULL DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COMMENT='订单表';
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(20) DEFAULT NULL COMMENT '用户名',
+  `password` varchar(64) DEFAULT NULL COMMENT '密码',
+  `avatar` longtext COMMENT '头像',
+  `phone` varchar(20) DEFAULT NULL COMMENT '电话',
+  `sign` varchar(300) DEFAULT NULL COMMENT '签名',
+  `createTime` datetime DEFAULT NULL COMMENT '创建时间',
+  `updateTime` datetime DEFAULT NULL COMMENT '更新时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+SET FOREIGN_KEY_CHECKS = 1;
